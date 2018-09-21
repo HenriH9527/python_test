c57e719ce129b42ce82f60b1b8dece7959ecbe5c
Ash huang <33093191+VincentHyf@users.noreply.github.com>
2018-09-21 10:18:46 +0800

    Create game_for.py

---
+++ python_function/bases/game_for.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Auther:Awe H
+
+count = 0
+age_of_oldboy = 56
+
+for i in range(3):
+    guess_age = int(input('guess age:'))
+    if guess_age == age_of_oldboy:
+        print('yes,you got it')
+        break
+    elif guess_age > age_of_oldboy:
+        print('think smaller...')
+    else:
+        print('think bigger')
+else:
+    print('you have tried too many times...fuck off')
+    
